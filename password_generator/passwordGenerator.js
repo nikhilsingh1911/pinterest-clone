46ae52aeacd29f57b82a7af922d0812d59110a19
nikhilsingh1911 <115785542+nikhilsingh1911@users.noreply.github.com>
2024-01-28 23:50:23 +0530

    p**k

---
+++ password_generator/passwordGenerator.js
@@ -0,0 +1,49 @@
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "1234567890";
+const symbol = "~!@#$%^&*()-";
+const passBox = document.querySelector("#mainText");
+
+const btn = document.querySelector("#generate");
+const upperInput = document.querySelector("#uppercase");
+const lowerInput = document.querySelector("#lowercase");
+const numberInput = document.querySelector("#numbers");
+const symbolInput = document.querySelector("#symbols");
+const passlength = document.querySelector("#passlength");
+
+// console.log(password)
+const getRandomData = (dataSet) => {
+    return dataSet[Math.floor(Math.random() * dataSet.length)]
+}
+const lengt = 0;
+const generatePassword = (password= "") => {
+    if (upperInput.checked) {
+        password += getRandomData(upperCase);
+    }
+    if (lowerInput.checked) {
+        password += getRandomData(lowerCase);
+    }
+    if (numberInput.checked) {
+        password += getRandomData(number);
+    }
+    if (symbolInput.checked) {
+        password += getRandomData(symbol);
+    }
+    if (password.length < passlength.value) {
+        return generatePassword(password);
+    }
+    passBox.innerText = truncateString(password, totalChar.value);
+}
+generatePassword();
+btn.addEventListener("click", function() {
+        generatePassword();
+    }
+)
+function truncateString(str, num) {
+    if (str.length > num) {
+        let subStr = str.substring(0, num);
+        return subStr;
+    } else {
+        return str;
+    }
+}
