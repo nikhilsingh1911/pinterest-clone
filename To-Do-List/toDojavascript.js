32e4773a8c0d77506941ce1d865d1f7e9c5b6475
nikhilsingh1911 <115785542+nikhilsingh1911@users.noreply.github.com>
2024-02-12 01:48:39 +0530

    kuch to kmal hua h bs pta ni kya kmal h😂😂😂😂

---
+++ To-Do-List/toDojavascript.js
@@ -0,0 +1,45 @@
+const magicDom = document.querySelector('.magic');
+const inputTask = document.querySelector("#toDotext");
+const addBtn = document.querySelector(".btn");
+
+
+
+const createNewTask = () => {
+    let inputDiv = document.createElement("div");
+
+
+    let doneBtn = document.createElement('button1');
+    doneBtn.innerHTML = 'O';
+    doneBtn.classList.add("donebtn");
+
+    let deleteBtn = document.createElement('button2');
+    deleteBtn.innerHTML = "x";
+    deleteBtn.classList.add("deleBtn");
+    if (inputTask.value === '') {
+        alert("You have to enter the first")
+    } else {
+        inputDiv.classList.add("inputdiv");
+
+        inputDiv.innerHTML = inputTask.value;
+        magicDom.appendChild(inputDiv);
+        inputDiv.appendChild(doneBtn);
+        inputDiv.append(deleteBtn);
+        inputTask.value = "";
+    }
+
+}
+magicDom.addEventListener("click", (e) => {
+    if (e.target.tagName === "BUTTON2") {
+        e.target.parentElement.remove();
+    }
+});
+magicDom.addEventListener("click", (e) => {
+    if (e.target.tagName === "BUTTON1") {
+        e.target.parentElement.classList.add("textline");
+    }
+});
+
+
+addBtn.addEventListener("click", () => {
+    createNewTask();
+})
