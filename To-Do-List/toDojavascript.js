6ca3878ba7700a85a22fc3250ba3524a43a9ccb4
nikhilsingh1911 <115785542+nikhilsingh1911@users.noreply.github.com>
2024-02-12 10:06:16 +0530

    done todo

--- To-Do-List/toDojavascript.js
+++ To-Do-List/toDojavascript.js
@@ -19,25 +19,32 @@ const createNewTask = () => {
         alert("You have to enter the first")
     } else {
         inputDiv.classList.add("inputdiv");
-
         inputDiv.innerHTML = inputTask.value;
         magicDom.appendChild(inputDiv);
         inputDiv.appendChild(doneBtn);
         inputDiv.append(deleteBtn);
-        inputTask.value = "";
     }
-
+    inputTask.value = "";
 }
+
+
 magicDom.addEventListener("click", (e) => {
     if (e.target.tagName === "BUTTON2") {
         e.target.parentElement.remove();
     }
 });
+
+
 magicDom.addEventListener("click", (e) => {
+    clickcount = 0;
     if (e.target.tagName === "BUTTON1") {
-        e.target.parentElement.classList.add("textline");
+        if(clickcount===0){
+            e.target.parentElement.style.background = 'rgb(7 217 42)';
+            clickcount++;
+        }
     }
 });
+
 
 
 addBtn.addEventListener("click", () => {
